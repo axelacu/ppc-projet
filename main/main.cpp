2c13fbe131499313f041a38446196f45bbf3b0fe
HAMICI-MohamedBoudjemaa <hamici.medboudjemaa@gmail.com>
2020-05-21 17:18:07 +0200

    add Main

--- main/main.cpp
+++ main/main.cpp
@@ -3,90 +3,14 @@
 #include <dlfcn.h>
 #include <stdio.h>
 #include <string>
+#include "Bloc.h"
+#include "Composant4.h"
+#include "Composant3.h"
 
-
-static int operation(int a, int b, std::string compo)
-{
-        int (*func)(int,int);
-        int t = 0;
-        void *handle;
-        
-        //std::cout << std::endl << "Loading Handler";
-        // Ouverture de la bibliothèque
-        if(compo.compare("Composant2")==0)
-                handle = dlopen("./libComposant2.so", RTLD_LAZY);
-        else
-                 handle = dlopen("./libComposant1.so", RTLD_LAZY);
-        if(handle == NULL)
-        {
-                printf("erreur dlopen : %s\n", dlerror());
-                exit(EXIT_FAILURE);
-        }
-
-        // Chargement de la fonction "func"
-        if(compo.compare("Composant2") == 0)
-        {
-                *(void **) (&func) = dlsym(handle, "composant2");
-                 //std::cout << std::endl << "Call function composant2";
-                if (func == NULL)
-                {
-                        printf("Error occured");
-                        printf("erreur dlsym : %s\n", dlerror());
-                        dlclose(handle);
-                        exit(EXIT_FAILURE);
-                }
-
-                // Exécution de la fonction "func"
-                t = func(a,b);
-                //std::cout << std::endl << " value return " << t << std::endl;
-        }
-        else
-        {
-              *(void **) (&func) = dlsym(handle, "composant1");
-                //std::cout << std::endl << "Call function composant1";
-                if (func == NULL)
-                {
-                        printf("Error occured");
-                        printf("erreur dlsym : %s\n", dlerror());
-                        dlclose(handle);
-                        exit(EXIT_FAILURE);
-                }
-
-                // Exécution de la fonction "func"
-                t = func(a,b);  
-                std::cout << std::endl << " value return " << t << std::endl;
-        }
-        // Fermeture de la bibliothèque
-        dlclose(handle);
-
-        return t;
-}
 int main(int argc, char ** argv)
 {
-    if(argc != 4) exit(EXIT_FAILURE);
-	int data1 = std::stoi(argv[2]);
-	int data2 = std::stoi(argv[3]);
-
-	int valeur=0;
-        std::cout << argc << " " << argv[1];
-        if(argc == 4)
-        {
-                std::string arg1(argv[1]);
-                //std::cout << std::endl << "In the loop " << arg1;
-                if(arg1.compare("Composant1") == 0)
-                {
-                        //std::cout << std::endl << "into component 1";
-                        valeur = operation(data1, data2, arg1);
-                }
-                else if(arg1.compare("Composant2") == 0)
-                {
-                        //std::cout << std::endl << "into component 2";
-                        valeur = operation(data1, data2, arg1);
-                }
-        }
-
-        std::cout << std::endl;
-	//std::cout << getComposant1Version() << std::endl;
-	std::cout << "valeur :" << valeur << std::endl;
+    Bloc bloc();
+    Composant3::composant3(bloc);
+    cout << bloc.getNonce() << '\n';
+    cout << bloc.getHash();
 }
-
