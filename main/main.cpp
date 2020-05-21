2bdd83b1a8c3fad674f896a734da34fbb7a2a514
HAMICI-MohamedBoudjemaa <hamici.medboudjemaa@gmail.com>
2020-05-22 00:09:40 +0200

    makefile

--- main/main.cpp
+++ main/main.cpp
@@ -13,9 +13,12 @@ int main(int argc, char ** argv)
     std::cout<<"Hello World"<<std::endl;
     //Bloc bloc;
     Bloc bloc;
-
+    std::cout<<"bonjour"<<std::endl;
     composant3(bloc);
-    std::cout << bloc.getNonce() << '\n';
-    std::cout << bloc.getHash();
+    std::cout<<"bonsoir"<<std::endl;
+    std::cout << bloc.getNonce() << '\n';    
+    std::cout<<"bonsoir1"<<std::endl;
+    std::cout << bloc.getHash();    
+    std::cout<<"bonsoir2"<<std::endl;
 
 }
