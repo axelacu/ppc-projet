9427485dc4e40de46ee926923f51ca6aebb2a57d
HAMICI-MohamedBoudjemaa <hamici.medboudjemaa@gmail.com>
2020-05-22 00:04:29 +0200

    makefile

--- main/main.cpp
+++ main/main.cpp
@@ -3,9 +3,9 @@
 #include <dlfcn.h>
 #include <stdio.h>
 #include <string>
-#include "Bloc.h"
-#include "Composant4.h"
-#include "Composant3.h"
+#include "Bloc.cpp"
+#include "Composant4.cpp"
+#include "Composant3.cpp"
 
 int main(int argc, char ** argv)
 {	
