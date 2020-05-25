8fb177f5fd80bfe5a1e5c3b2da54c359623738c0
Ravoavy <36300959+chrisrhcm@users.noreply.github.com>
2020-05-25 11:35:54 +0200

    Update Composant5.h

--- interfaces/Composant5.h
+++ interfaces/Composant5.h
@@ -8,6 +8,10 @@ extern "C" int composant5(int p1, int p2);
 
 class Verificateur_Bloc {
 public:
+    //venant de composant 6
+    static bool verify(std::string pubkey, char hash[HASH_SIZE], unsigned char signature[64]);
+    //venant de composant 4
+    static bool verifHash(Bloc totest, char hashtotest[HASH_SIZE]);
     static bool VerificateurHash(Bloc bloc);
     static bool VerificateurDifficulte(Bloc bloc);
     static bool VerificateurTransaction(std::string pubkey, Bloc bloc);
