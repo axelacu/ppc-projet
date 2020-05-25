2c68fb7e9929704e7940b5b58f15a2a150134092
NGYMich <ngmich95@gmail.com>
2020-05-25 09:58:08 +0200

    Composant 5 push

---
+++ Composant5/Verificateur_Bloc.cpp
@@ -0,0 +1,60 @@
+#include <string>
+#include "Verificateur_Bloc.h"
+#include <iostream>
+#include "Bloc.h"
+using namespace std;
+
+int difficulte = 4;
+
+bool Verificateur_Bloc::VerificateurBloc(std::string pubkey, Bloc bloc) {
+    return VerificateurHash(bloc) && VerificateurDifficulte(bloc) && VerificateurTransaction(pubkey, bloc);
+}
+
+bool Verificateur_Bloc::VerificateurTransaction(std::string pubkey, Bloc bloc) {
+    std::vector<TXI> entree = bloc.tx1.TXIs;
+    bool testEntree;
+    for(int i = 0; i<entree.size(); i++){
+        testEntree = testEntree && verify(pubkey, bloc.hash, entree.at(i).signature);
+    }
+    if(!testEntree) {
+        cerr << "transaction incorrecte";
+        return false;
+    }
+    else
+        return true;
+}
+
+bool Verificateur_Bloc::VerificateurDifficulte(Bloc bloc) {
+    bool difficulteOk;
+    for (int i=0; i<4; i++) {
+        if (bloc.hash[i] =! '0')
+            difficulteOk = difficulteOk && false;
+    }
+    if (!difficulteOk) {
+        cerr << "difficulte incorrecte";
+        return false;
+    }
+    else return true;
+}
+
+bool Verificateur_Bloc::VerificateurHash(Bloc bloc) {
+    bool hashOk = verifHash(bloc, bloc.hash);
+    if (!hashOk) {
+        cerr << "hash incorrect";
+        return false;
+    }
+    return true;
+}
+
+bool Verificateur_Bloc::verifHash(Bloc totest, char *hashtotest) {
+    return true;
+}
+
+bool Verificateur_Bloc::verify(std::string pubkey, char *hash, unsigned char *signature) {
+    return true;
+}
+
+int main (int argc, char** argv)
+{
+    std::cout << "Test";
+}
--- interfaces/Composant5.h
+++ interfaces/Composant5.h
@@ -1,7 +1,19 @@
-#ifndef COMPOSANT2_H
-#define COMPOSANT2_H
+#ifndef COMPOSANT5_H
+#define COMPOSANT5_H
 
 // interface du composant2
 
 extern "C" int composant2(int p1, int p2);
+
+
+class Verificateur_Bloc {
+public:
+    static bool verify(std::string pubkey, char hash[HASH_SIZE], unsigned char signature[64]);
+    static bool verifHash(Bloc totest, char hashtotest[HASH_SIZE]);
+    static bool VerificateurHash(Bloc bloc);
+    static bool VerificateurDifficulte(Bloc bloc);
+    static bool VerificateurTransaction(std::string pubkey, Bloc bloc);
+    static bool VerificateurBloc(std::string pubkey, Bloc bloc);
+};
+
 #endif
